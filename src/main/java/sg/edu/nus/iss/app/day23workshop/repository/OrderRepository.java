b2d9d56de3d7da1d0a12f9364bf6589e47d9305e
haha27lim <eileen2708@hotmail.com>
2023-02-22 21:31:19 +0800

    updated

--- src/main/java/sg/edu/nus/iss/app/day23workshop/repository/OrderRepository.java
+++ src/main/java/sg/edu/nus/iss/app/day23workshop/repository/OrderRepository.java
@@ -2,15 +2,12 @@ package sg.edu.nus.iss.app.day23workshop.repository;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import sg.edu.nus.iss.app.day23workshop.model.Order;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import static sg.edu.nus.iss.app.day23workshop.repository.Queries.*;
 
@@ -21,12 +18,12 @@ public class OrderRepository {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    // method to retrieve order details by their order ID
     public Order getOrderDetails(Integer orderId) {
        
-        
+        // execute the query to retrieve the order details by order ID
         return jdbcTemplate.queryForObject(SQL_SELECT_ORDERS_DETAILS_BY_ID, BeanPropertyRowMapper.newInstance(Order.class), orderId);
     
-        
 
     }
 }
